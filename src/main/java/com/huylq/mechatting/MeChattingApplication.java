bdf63814cb80dfc3d9278bf2b2472f327d860d8b
javah <huylq4198@gmail.com>
2022-12-17 00:21:27 +0700

    init project

---
+++ src/main/java/com/huylq/mechatting/MeChattingApplication.java
@@ -0,0 +1,13 @@
+package com.huylq.mechatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeChattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huylq/mechatting/MeChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huylq.mechatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
